c7061f3e6b5b0cec9b2513733a6e3a63acf6a548
Volodymyr <volodymyrquo@gmail.com>
2021-01-14 08:02:23 +0200

    created structure of  project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Contacts,
+  Footer,
+  Home,
+  Navbar,
+  News,
+  NotFoundPage,
+} from './components';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/news' component={News} />
+        <Route path='/contacts' component={Contacts} />
+        <Route component={NotFoundPage} />
+      </Switch>
+      <Footer />
+    </>
   );
 };
 
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Navbar } from './Navbar';
+export { default as Footer } from './Footer';
+export { default as Home } from './Home';
+export { default as Contacts } from './Contacts';
+export { default as News } from './News';
+export { default as NotFoundPage } from './NotFoundPage';
+export { default as Details } from './Details';
